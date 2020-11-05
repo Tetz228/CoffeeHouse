48107ca42e38cc201afffc4959ac7a9f7b0acd33
Tetz <tetz2281337@gmail.com>
2020-11-05 21:48:12 +0700

    Удаление лишних библиотек

--- WpfApp1/MyProfileWindow.xaml.cs
+++ WpfApp1/MyProfileWindow.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
