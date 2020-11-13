a2e0f976be38da7032424b816e7880edeb1f5a24
Tetz <tetz2281337@gmail.com>
2020-11-13 20:07:00 +0700

    Перенос menu в окно официанта

---
+++ WpfApp1/OrdersAndReportUserControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public partial class OrdersAndReportUserControl : UserControl
+    {
+        private readonly ActionsOrders actionsOrders;
+
+        private int IdUser { get; }
+
+        public OrdersAndReportUserControl(int idUser)
+        {
+            InitializeComponent();
+
+            IdUser = idUser;
+            actionsOrders = new ActionsOrders(IdUser);
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataGridOrders.ItemsSource = actionsOrders.OutputOrders();
+        }
+
+        private void DataGridOrders_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (DataGridOrders.SelectedItem != null)
+            {
+                Order order = DataGridOrders.SelectedItem as Order;
+
+                AddOrderWindow addOrder = new AddOrderWindow(order, IdUser);
+                addOrder.ShowDialog();
+
+                DataGridOrders.ItemsSource = actionsOrders.OutputOrders();
+            }            
+        }
+
+        private void DataGridOrders_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DataGridOrders.SelectedItem = null;
+        }
+        
+        public void GoToReportShift()
+        {            
+            DataGridOrders.ItemsSource = actionsOrders.OutputReportShiftEmployee();
+            DataGridOrders.IsEnabled = false;
+        }
+
+        public void GoToWaiterWindow()
+        {
+            DataGridOrders.ItemsSource = actionsOrders.OutputOrders();
+            DataGridOrders.IsEnabled = true;
+        }
+
+        public void GoToCashOrderWindow()
+        {            
+            if (DataGridOrders.SelectedItem != null)
+            {
+                Order order = DataGridOrders.SelectedItem as Order;
+
+            }
+            else
+            {
+                MessageBox.Show("Выберите заказ для формирования приходно-кассового ордера");
+            }
+        }
+    }
+}
--- WpfApp1/WaiterWindow.xaml.cs
+++ WpfApp1/WaiterWindow.xaml.cs
@@ -4,6 +4,10 @@ namespace WpfApp1
 {
     public partial class WaiterWindow : Window
     {
+        private readonly ActionsOrders actionsOrders;
+
+        private readonly OrdersAndReportUserControl reportsProfileUserControl; 
+
         private int IdUser { get; }
 
         public WaiterWindow(int idUser)
@@ -11,11 +15,71 @@ namespace WpfApp1
             InitializeComponent();
 
             IdUser = idUser;
+            actionsOrders = new ActionsOrders(idUser);
+            reportsProfileUserControl = new OrdersAndReportUserControl(idUser);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            MainControl.Content = new OrdersReportsProfileUserControl(IdUser);          
+            MainControl.Content = reportsProfileUserControl;
+            MenuItemUser.Header = actionsOrders.GettingLFMEmployee();
+        }
+
+        private void AddOrder_Click(object sender, RoutedEventArgs e)
+        {
+            AddOrderWindow addOrderWindow = new AddOrderWindow(IdUser);
+            addOrderWindow.ShowDialog();
+
+            reportsProfileUserControl.GoToWaiterWindow();
+        }
+
+        private void MenuItemLogOutAccount_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();            
+            authWindow.Show();
+
+            Close();
+        }
+
+        private void MenuItemProfile_Click(object sender, RoutedEventArgs e)
+        {                                                               //Создать метод, который получает название должности
+            MyProfileWindow myProfile = new MyProfileWindow(IdUser, "Официант");
+            myProfile.ShowDialog();
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItemReport_Click(object sender, RoutedEventArgs e)
+        {
+            reportsProfileUserControl.GoToReportShift();
+
+            GoToWaiterWindow.Visibility = Visibility.Visible;
+            AddOrder.Visibility = Visibility.Collapsed;
+            MenuItemUser.Visibility = Visibility.Collapsed;
+            MenuItemCashOrder.Visibility = Visibility.Collapsed;
+
+            Title = "Окно официанта -> Отчет за смену";    
+        }
+
+        private void GoToWaiterWindow_Click(object sender, RoutedEventArgs e)
+        {
+            reportsProfileUserControl.GoToWaiterWindow();
+
+            GoToWaiterWindow.Visibility = Visibility.Collapsed;
+            AddOrder.Visibility = Visibility.Visible;
+            MenuItemUser.Visibility = Visibility.Visible;
+            MenuItemCashOrder.Visibility = Visibility.Visible;
+
+            Title = "Окно официанта -> Список заказов";
+        }
+
+        private void MenuItemCashOrder_Click(object sender, RoutedEventArgs e)
+        {
+            reportsProfileUserControl.GoToCashOrderWindow();
+            
         }
     }
 }
